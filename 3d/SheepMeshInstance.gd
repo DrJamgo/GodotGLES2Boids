75edd98d84a33bedaac219e35fe62922f9a3929c
Dr.Jamgo <drjamgo@hotmail.com>
2023-02-10 19:59:10 +0100

    FEAT: Sheep feeding makes them "glow"

--- 3d/SheepMeshInstance.gd
+++ 3d/SheepMeshInstance.gd
@@ -10,14 +10,25 @@ func _ready():
 
         multimesh.instance_count = boids_multimesh.instance_count
         
-        for index in range(0, boids_multimesh.instance_count):
-            multimesh.set_instance_custom_data(index, boids_multimesh.get_instance_custom_data(index))
-        
         var shader := (material_override as ShaderMaterial)
+        var tile_size = (shader.get_shader_param("tex") as Texture).get_size() / (multimesh.mesh as QuadMesh).size
+        tile_size
+        for index in range(0, boids_multimesh.instance_count):
+            var custom_data : Color = boids_multimesh.get_instance_custom_data(index)
+            # enable to make random
+            # custom_data.b = float(randi() % 4) / tile_size.y
+            if index == 0:
+                custom_data.b = 3.0 / tile_size.y
+            else:
+                custom_data.b = 1.0 / tile_size.y
+                
+            multimesh.set_instance_custom_data(index, custom_data)
+        
         shader.set_shader_param("grid_size", boids_spec.grid_size)
         shader.set_shader_param("world_size", boids_spec.world_size)
         shader.set_shader_param("state_texture", boids_instance.particles_texture)
         shader.set_shader_param("velocity_max", boids_spec.velocity_max)
+        shader.set_shader_param("tile_size", tile_size)
     
 func _process(delta):
     var boids_instance : BoidsInstance = get_node(boids_instance_path)
